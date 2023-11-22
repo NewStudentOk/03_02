e61f2b4485f6cf9cd41226aee26f9822bd14a574
“Studen” <Studen>
2023-11-22 20:18:32 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+class Counter {
+private:
+    int start;
+public:
+    std::string answer;
+    std::string oper;
+
+    Counter(std::string answer, std::string oper, int start) {
+        this->answer = answer;
+        this->oper = oper;
+        this->start = start;
+    }
+
+    Counter() {
+        answer = "Неизвестно";
+        oper = "Неизвестно";
+        start = 1;
+    }
+
+    int getStart() const //геттер смотрим значение поля start
+    {
+        return start;
+    }
+
+    void setStart(int value) //сеттер приравняем значение поле start к переменной value при этом поле start не изменится в классе
+    {
+        start = value;
+    }
+
+    int add() {
+        start = start + 1;
+        return start;
+    }
+
+    int subtract_start_1() {
+        start = start - 1;
+        return start;
+    };
+
+    int print() const {
+        return start;
+    }
+
+};
+
+int main() {
+    Counter counter;
+    int value;
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    std::cin >> counter.answer;
+    if (counter.answer == "да") {
+        std::cout << "Введите начальное значение счётчика: ";
+        std::cin >> value;
+        counter.setStart(value);
+    } else {
+        std::cout << "Начальное значение счетчика по умолчанию: ";
+        value = counter.getStart();
+        std::cout << value << std::endl;
+    }
+    do {
+        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> counter.oper;
+        if (counter.oper == "+") {
+            counter.add();
+        }
+        if (counter.oper == "-") {
+            counter.subtract_start_1();
+        }
+        if (counter.oper == "=") {
+            std::cout << counter.print() << std::endl;
+        }
+    } while (counter.oper != "х");
+    return 0;
+}
